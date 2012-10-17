8cbaf747ccd03b91179055a03e9480f5a8a1f140
Thomas Langaas <thomas.langas@fxitech.com>
2012-10-17 12:19:12 +0200

    Cleans up whitespace
    Change-Id: I38f7c9599b5cba9a6db610786adfc03f82d5bf16

--- arch/arm/mach-exynos/fxi-sysfs.c
+++ arch/arm/mach-exynos/fxi-sysfs.c
@@ -1,7 +1,7 @@
 //--------------------------------------------------------------------------------------------------
 //  HKC1XX Board : HKC1XX sysfs driver (charles.park)
 //  2010.04.20
-//   
+//
 //  FXI USB Dongle Sysfs Driver Update (icarus)
 //  2011.07.06
 //
@@ -28,90 +28,88 @@
 #define	SLEEP_DISABLE_FLAG
 
 #if defined(SLEEP_DISABLE_FLAG)
-	#ifdef CONFIG_HAS_WAKELOCK
-		#include <linux/wakelock.h>
-		static struct wake_lock 	sleep_wake_lock;
-	#endif
+#ifdef CONFIG_HAS_WAKELOCK
+#include <linux/wakelock.h>
+static struct wake_lock	sleep_wake_lock;
+#endif
 #endif
 
 // GPIO Index Define
-enum	{
+enum {
 	// Bluetooth
 	BLUETOOTH_BUTTON,
-	
+
 	// 3G Modem Control Port
 	MODEM_POWER,
 	MODEM_RESET,
 	MODEM_DISABLE1,
 	MODEM_DISABLE2,
-	
+
 	// Status LED Display
 	STATUS_LED_RED,
 	STATUS_LED_GREEN,
 	STATUS_LED_BLUE,
 	STATUS_LED_NETRED,
 	STATUS_LED_NETGREEN,
-	
+
 	// Power Control
 //	SYSTEM_POWER_3V3,		// BUCK6 Enable Control
 	SYSTEM_POWER_5V0,			// USB HOST Power
 	SYSTEM_POWER_12V0,		// VLED Control (Backlight)
-	
+
 	GPIO_INDEX_END
 };
 
 static struct {
-	int		gpio_index;	// Control Index
-	int 	gpio;				// GPIO Number
-	char	*name;			// GPIO Name == sysfs attr name (must)
-	bool 	output;			// 1 = Output, 0 = Input
-	int 	value;			// Default Value(only for output)
-	int		pud;				// Pull up/down register setting : S3C_GPIO_PULL_DOWN, UP, NONE
+	int gpio_index;	// Control Index
+	int gpio;				// GPIO Number
+	char *name;			// GPIO Name == sysfs attr name (must)
+	bool output;			// 1 = Output, 0 = Input
+	int value;			// Default Value(only for output)
+	int pud;				// Pull up/down register setting : S3C_GPIO_PULL_DOWN, UP, NONE
 } sControlGpios[] = {
-	{	BLUETOOTH_BUTTON,			EXYNOS4_GPX3(5), "bt_button",		0, 0, S3C_GPIO_PULL_UP	},
+	{	BLUETOOTH_BUTTON,	EXYNOS4_GPX3(5), "bt_button",		0, 0, S3C_GPIO_PULL_UP},
 
 	// High -> Power Enable
-	{	MODEM_POWER,			EXYNOS4_GPX1(0), "modem_power", 		1, 1, S3C_GPIO_PULL_UP	},
+	{	MODEM_POWER,		EXYNOS4_GPX1(0), "modem_power",		1, 1, S3C_GPIO_PULL_UP},
 	// Low -> Reset Active
-	{	MODEM_RESET,			EXYNOS4_GPX1(1), "modem_reset", 		1, 0, S3C_GPIO_PULL_UP	},
+	{	MODEM_RESET,		EXYNOS4_GPX1(1), "modem_reset",		1, 0, S3C_GPIO_PULL_UP},
 	// High -> Disable1 Active
-	{	MODEM_DISABLE1,			EXYNOS4_GPX1(2), "modem_disable1",		1, 0, S3C_GPIO_PULL_UP	},
+	{	MODEM_DISABLE1,		EXYNOS4_GPX1(2), "modem_disable1",	1, 0, S3C_GPIO_PULL_UP},
 	// High -> Disable2 Active
-	{	MODEM_DISABLE2,			EXYNOS4_GPX1(3), "modem_disable2",		1, 0, S3C_GPIO_PULL_UP	},
-	
+	{	MODEM_DISABLE2,		EXYNOS4_GPX1(3), "modem_disable2",	1, 0, S3C_GPIO_PULL_UP},
+
 	// STATUS LED : High -> LED ON
-	{	STATUS_LED_RED,		EXYNOS4_GPC0(4), "led_red",			1, 0, S3C_GPIO_PULL_DOWN	},
-	{	STATUS_LED_GREEN,			EXYNOS4_GPC0(3), "led_green",				1, 0, S3C_GPIO_PULL_DOWN	},
+	{	STATUS_LED_RED,		EXYNOS4_GPC0(4), "led_red",		1, 0, S3C_GPIO_PULL_DOWN},
+	{	STATUS_LED_GREEN,	EXYNOS4_GPC0(3), "led_green",		1, 0, S3C_GPIO_PULL_DOWN},
+	{	STATUS_LED_BLUE		EXYNOS4_GPC0(2), "led_blue",		1, 0, S3C_GPIO_PULL_DOWN},
+	{	STATUS_LED_NETRED,	EXYNOS4_GPC0(1), "led_netred",		1, 0, S3C_GPIO_PULL_DOWN},
+	{	STATUS_LED_NETGREEN,	EXYNOS4_GPC1(0), "led_netgreen",	1, 0, S3C_GPIO_PULL_DOWN},
 
-	{	STATUS_LED_BLUE,		EXYNOS4_GPC0(2), "led_blue",			1, 0, S3C_GPIO_PULL_DOWN	},
-
-	{	STATUS_LED_NETRED,	EXYNOS4_GPC0(1), "led_netred",		1, 0, S3C_GPIO_PULL_DOWN	},
-	{	STATUS_LED_NETGREEN,EXYNOS4_GPC1(0), "led_netgreen",	1, 0, S3C_GPIO_PULL_DOWN	},
-	
 	// SYSTEM POWER CONTROL
-//	{	SYSTEM_POWER_3V3,		EXYNOS4_GPX3(5), "power_3v3",			1, 1, S3C_GPIO_PULL_DOWN	},
-	{	SYSTEM_POWER_5V0,		EXYNOS4_GPC0(0), "power_5v0",			1, 1, S3C_GPIO_PULL_DOWN	},
-	{	SYSTEM_POWER_12V0,	EXYNOS4_GPC0(3), "power_12v0",			1, 1, S3C_GPIO_PULL_DOWN	},
+//	{	SYSTEM_POWER_3V3,	EXYNOS4_GPX3(5), "power_3v3",		1, 1, S3C_GPIO_PULL_DOWN},
+	{	SYSTEM_POWER_5V0,	EXYNOS4_GPC0(0), "power_5v0",		1, 1, S3C_GPIO_PULL_DOWN},
+	{	SYSTEM_POWER_12V0,	EXYNOS4_GPC0(3), "power_12v0",		1, 1, S3C_GPIO_PULL_DOWN},
 
 };
 
 
 //   function prototype define
-static 	int		fxi_sysfs_resume		(struct platform_device *dev);
-static 	int		fxi_sysfs_suspend	(struct platform_device *dev, pm_message_t state);
-static	int		fxi_sysfs_probe		(struct platform_device *pdev);
-static	int		fxi_sysfs_remove		(struct platform_device *pdev);	
+static int fxi_sysfs_resume(struct platform_device *dev);
+static int fxi_sysfs_suspend(struct platform_device *dev, pm_message_t state);
+static int fxi_sysfs_probe(struct platform_device *pdev);
+static int fxi_sysfs_remove(struct platform_device *pdev);
 
-static 	int 	__init fxi_sysfs_init(void);
-static 	void 	__exit fxi_sysfs_exit(void);
+static int	__init fxi_sysfs_init(void);
+static void	__exit fxi_sysfs_exit(void);
 
 static struct platform_driver fxi_sysfs_driver = {
 	.driver = {
 		.name = "fxi-sysfs",
 		.owner = THIS_MODULE,
 	},
-	.probe 		= fxi_sysfs_probe,
-	.remove 	= fxi_sysfs_remove,
+	.probe		= fxi_sysfs_probe,
+	.remove		= fxi_sysfs_remove,
 	.suspend	= fxi_sysfs_suspend,
 	.resume		= fxi_sysfs_resume,
 };
@@ -127,27 +125,27 @@ MODULE_LICENSE("GPL");
 extern	bool s5p_hpd_get_status(void);
 
 //   sysfs function prototype define
-static	ssize_t show_gpio	(struct device *dev, struct device_attribute *attr, char *buf);
-static 	ssize_t set_gpio	(struct device *dev, struct device_attribute *attr, const char *buf, size_t count);
-static	ssize_t show_hdmi	(struct device *dev, struct device_attribute *attr, char *buf);
+static	ssize_t show_gpio(struct device *dev, struct device_attribute *attr, char *buf);
+static	ssize_t set_gpio(struct device *dev, struct device_attribute *attr, const char *buf, size_t count);
+static	ssize_t show_hdmi(struct device *dev, struct device_attribute *attr, char *buf);
 
-static	DEVICE_ATTR(bt_button,		S_IRWXUGO, show_gpio, set_gpio);
+static	DEVICE_ATTR(bt_button, S_IRWXUGO, show_gpio, set_gpio);
 
-static	DEVICE_ATTR(modem_power, 		S_IRWXUGO, show_gpio, set_gpio);
-static	DEVICE_ATTR(modem_reset,		S_IRWXUGO, show_gpio, set_gpio);
-static	DEVICE_ATTR(modem_disable1,		S_IRWXUGO, show_gpio, set_gpio);
-static	DEVICE_ATTR(modem_disable2,		S_IRWXUGO, show_gpio, set_gpio);
+static	DEVICE_ATTR(modem_power, S_IRWXUGO, show_gpio, set_gpio);
+static	DEVICE_ATTR(modem_reset, S_IRWXUGO, show_gpio, set_gpio);
+static	DEVICE_ATTR(modem_disable1, S_IRWXUGO, show_gpio, set_gpio);
+static	DEVICE_ATTR(modem_disable2, S_IRWXUGO, show_gpio, set_gpio);
 
-static	DEVICE_ATTR(led_red, 			S_IRWXUGO, show_gpio, set_gpio);
-static	DEVICE_ATTR(led_green,			S_IRWXUGO, show_gpio, set_gpio);
-static	DEVICE_ATTR(led_blue,			S_IRWXUGO, show_gpio, set_gpio);
-static	DEVICE_ATTR(led_netred,			S_IRWXUGO, show_gpio, set_gpio);
-static	DEVICE_ATTR(led_netgreen,			S_IRWXUGO, show_gpio, set_gpio);
+static	DEVICE_ATTR(led_red, S_IRWXUGO, show_gpio, set_gpio);
+static	DEVICE_ATTR(led_green, S_IRWXUGO, show_gpio, set_gpio);
+static	DEVICE_ATTR(led_blue, S_IRWXUGO, show_gpio, set_gpio);
+static	DEVICE_ATTR(led_netred, S_IRWXUGO, show_gpio, set_gpio);
+static	DEVICE_ATTR(led_netgreen, S_IRWXUGO, show_gpio, set_gpio);
 
-static	DEVICE_ATTR(power_5v0,			S_IRWXUGO, show_gpio, set_gpio);
-static	DEVICE_ATTR(power_12v0,			S_IRWXUGO, show_gpio, set_gpio);
+static	DEVICE_ATTR(power_5v0, S_IRWXUGO, show_gpio, set_gpio);
+static	DEVICE_ATTR(power_12v0, S_IRWXUGO, show_gpio, set_gpio);
 
-static	DEVICE_ATTR(hdmi_state,			S_IRWXUGO, show_hdmi, NULL);
+static	DEVICE_ATTR(hdmi_state, S_IRWXUGO, show_hdmi, NULL);
 
 static struct attribute *fxi_sysfs_entries[] = {
 	&dev_attr_bt_button,
@@ -174,7 +172,7 @@ static struct attribute_group fxi_sysfs_attr_group = {
 	.attrs  = fxi_sysfs_entries,
 };
 
-static 	ssize_t show_gpio(struct device *dev, struct device_attribute *attr, char *buf)
+static ssize_t show_gpio(struct device *dev, struct device_attribute *attr, char *buf)
 {
 	int	i;
 
@@ -182,16 +180,17 @@ static 	ssize_t show_gpio(struct device *dev, struct device_attribute *attr, cha
 		if(!strcmp(sControlGpios[i].name, attr->attr.name))
 			return	sprintf(buf, "%d\n", (gpio_get_value(sControlGpios[i].gpio) ? 1 : 0));
 	}
-	
+
 	return	sprintf(buf, "ERROR! : GPIO not found!\n");
 }
 
 
-static 	ssize_t set_gpio(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
+static ssize_t set_gpio(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
 {
     unsigned int	val, i;
 
-    if(!(sscanf(buf, "%d\n", &val))) 	return	-EINVAL;
+    if(!(sscanf(buf, "%d\n", &val)))
+	return	-EINVAL;
 
 	for (i = 0; i < ARRAY_SIZE(sControlGpios); i++) {
 		if(!strcmp(sControlGpios[i].name, attr->attr.name))	{
@@ -209,10 +208,10 @@ static 	ssize_t set_gpio(struct device *dev, struct device_attribute *attr, cons
     return 0;
 }
 
-static	ssize_t show_hdmi	(struct device *dev, struct device_attribute *attr, char *buf)
+static ssize_t show_hdmi(struct device *dev, struct device_attribute *attr, char *buf)
 {
 	int status = gpio_get_value(EXYNOS4_GPX3(7));    //s5p_hpd_get_status();
-	
+
 	if(status)	return	sprintf(buf, "%s\n", "on");
 	else		return	sprintf(buf, "%s\n", "off");
 }
@@ -220,45 +219,44 @@ static	ssize_t show_hdmi	(struct device *dev, struct device_attribute *attr, cha
 void fxi_led_control(int led, int val)
 {
 	int index;
-	
+
 	switch (led) {
-		case	LED_RED:
-			index = STATUS_LED_RED;	
+		case LED_RED:
+			index = STATUS_LED_RED;
 			break;
-		case	LED_GREEN:
+		case LED_GREEN:
 			index = STATUS_LED_GREEN;
 			break;
-		case	LED_BLUE:
+		case LED_BLUE:
 			index = STATUS_LED_BLUE;
 			break;
-		case	LED_NETRED:
+		case LED_NETRED:
 			index = STATUS_LED_NETRED;
 			break;
-		case	LED_NETGREEN:
+		case LED_NETGREEN:
 			index = STATUS_LED_NETGREEN;
 			break;
 		default:
 			return;
 	}
-	
+
 	gpio_set_value(sControlGpios[index].gpio, ((val != 0) ? 1 : 0));
 }
 EXPORT_SYMBOL(fxi_led_control);
 
-void SYSTEM_POWER_CONTROL	(int power, int val)
+void SYSTEM_POWER_CONTROL(int power, int val)
 {
 	int	index;
-	
+
 	switch(power)	{
 //		case	0:	index = SYSTEM_POWER_3V3;		break;
 		case	1:	index = SYSTEM_POWER_5V0;		break;
 		case	2:	index = SYSTEM_POWER_12V0;		break;
 		default	:									return;
 	}
-	
+
 	gpio_set_value(sControlGpios[index].gpio, ((val != 0) ? 1 : 0));
 }
-
 EXPORT_SYMBOL(SYSTEM_POWER_CONTROL);
 
 static int fxi_sysfs_resume(struct platform_device *dev)
@@ -275,11 +273,11 @@ static int fxi_sysfs_suspend(struct platform_device *dev, pm_message_t state)
 	#if	defined(DEBUG_PM_MSG)
 		printk("%s\n", __FUNCTION__);
 	#endif
-	
+
     return  0;
 }
 
-static	int	fxi_sysfs_probe(struct platform_device *pdev)	
+static	int	fxi_sysfs_probe(struct platform_device *pdev)
 {
 	int	i;
 
@@ -308,11 +306,11 @@ static	int	fxi_sysfs_probe(struct platform_device *pdev)
 }
 
 
-static int fxi_sysfs_remove(struct platform_device *pdev)	
+static int fxi_sysfs_remove(struct platform_device *pdev)
 {
 	int	i;
-	
-	for (i = 0; i < ARRAY_SIZE(sControlGpios); i++) 	gpio_free(sControlGpios[i].gpio);
+
+	for (i = 0; i < ARRAY_SIZE(sControlGpios); i++)	gpio_free(sControlGpios[i].gpio);
 
 #if defined(SLEEP_DISABLE_FLAG)
 	#ifdef CONFIG_HAS_WAKELOCK
@@ -321,7 +319,7 @@ static int fxi_sysfs_remove(struct platform_device *pdev)
 #endif
 
     sysfs_remove_group(&pdev->dev.kobj, &fxi_sysfs_attr_group);
-    
+
     return	0;
 }
 
@@ -347,5 +345,4 @@ static void __exit fxi_sysfs_exit(void)
 #endif
     platform_driver_unregister(&fxi_sysfs_driver);
 }
-
 
